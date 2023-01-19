76bdc36f762aa34f89126664d618a664c3fc1907
marksavytskyi <dorozhe.zolota777@gmail.com>
2023-01-18 23:03:24 -0800

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+import Section from './Feedback/Section';
+import FeedbackOptions from './Feedback/FeedbackOptions';
+import Statistics from './Feedback/Statistics';
+import Notification from './Notification';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleFeedback = ({ currentTarget: { value } }) =>
+    this.setState(prevState => ({ [value]: prevState[value] + 1 }));
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+
+    const total = this.countTotalFeedback();
+
+    return `${Math.round((good * 100) / total)}%`;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const feedbackOptions = ['good', 'neutral', 'bad'];
+
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={feedbackOptions}
+            onLeaveFeedback={this.handleFeedback}
+          ></FeedbackOptions>
+        </Section>
+
+        <Section title="Statistics">
+          {this.countTotalFeedback() < 1 ? (
+            <Notification message="There is no feedback"></Notification>
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            ></Statistics>
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
